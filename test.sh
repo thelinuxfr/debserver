7f7526394eac64938153b6d4406c14595362ad53
Frédéric LIETART (TheLinuxFr) <stuff@thelinuxfr.org>
2016-05-31 23:04:11 +0200

    Minor fix test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 ### Fix docker
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 # setup docker modifications to NOT gzip indexes
-sed -i 's/Acquire::GzipIndexes "true"/Acquire::GzipIndexes "false"/g' /etc/apt/apt.conf.d/docker-gzip-indexes###
+sed -i 's/Acquire::GzipIndexes "true"/Acquire::GzipIndexes "false"/g' /etc/apt/apt.conf.d/docker-gzip-indexes
 
 chmod +x $1
 sed -i "s/LOCALES//g" $1
