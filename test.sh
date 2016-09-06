31b9d3c99c7a61e4b20cc9b6fbbc2a4f1ec29cb8
Frédéric LIETART (TheLinuxFr) <stuff@thelinuxfr.org>
2016-09-06 22:16:05 +0200

    Cleanup scripts and CI

--- test.sh
+++ test.sh
@@ -5,10 +5,10 @@ echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 # setup docker modifications to NOT gzip indexes
 sed -i 's/Acquire::GzipIndexes "true"/Acquire::GzipIndexes "false"/g' /etc/apt/apt.conf.d/docker-gzip-indexes
 
-chmod +x $1
-sed -i "s/LOCALES//g" $1
-sed -i "s/PROXY//g" $1
-sed -i "s/APTLISTBUGS//g" $1
-sed -i "s/NORECOMMENDS//g" $1
-sed -i "s/clear//g" $1
-yes | ./$1
+chmod +x "$1"
+sed -i "s/LOCALES//g" "$1"
+sed -i "s/PROXY//g" "$1"
+sed -i "s/APTLISTBUGS//g" "$1"
+sed -i "s/NORECOMMENDS//g" "$1"
+sed -i "s/clear//g" "$1"
+yes | ./"$1"
